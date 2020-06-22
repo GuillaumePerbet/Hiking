6bcd05c1e58e87dc3b5b8a3a1486afeae6a40858
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-22 15:57:02 +0200

    create excursion page init

---
+++ excursion.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("template/head.html"); ?>
+    <title>Excursions</title>
+</head>
+<body>
+    <header>
+        <?php include_once("template/navbar.html"); ?>
+    </header>
+    
+</body>
+</html>
