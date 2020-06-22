47ae78c4f216508ac1f7d997e1d9687729050949
Guillaume Perbet <perbet.dev@gmail.com>
2020-06-22 17:07:49 +0200

    import places and guides lists from database

--- excursion.php
+++ excursion.php
@@ -10,7 +10,23 @@
     </header>
     
     <main>
-        <?php include_once("dbconnect.php"); ?>
+        <?php include_once("dbconnect.php");
+            //fetch array of places 'id' and 'name' in $places
+            $sql = "SELECT id,name FROM place";
+            $req = $pdo->prepare($sql);
+            $req -> execute();
+            $places = $req->fetchAll();
+            $req -> closeCursor();
+
+            //fetch array of guide name in $guides
+            $sql = "SELECT id,last_name,first_name FROM guide";
+            $req = $pdo->prepare($sql);
+            $req -> execute();
+            $guides = $req->fetchAll();
+            $req -> closeCursor();
+        ?>
+
+
 
         <form action="formHandler/create_excursion.php" method="POST">
             <label>Nom de l'excursion
@@ -42,18 +58,20 @@
 
                 <label>Point de départ
                     <select name="departure_place">
-                        <option value="nord">Nord</option>
                         <?php
-                            //récupérer la liste des régions
+                            foreach($places as $place){
+                                echo "<option value='" . $place['id'] . "'>" . $place['name'] . "</option>";
+                            }
                         ?>
                     </select>
                 </label>
     
                 <label>Point d'arrivée
                     <select name="arrival_place">
-                        <option value="nord">Nord</option>
                         <?php
-                            //récupérer la liste des régions
+                            foreach($places as $place){
+                                echo "<option value='" . $place['id'] . "'>" . $place['name'] . "</option>";
+                            }
                         ?>
                     </select>
                 </label>
@@ -62,12 +80,13 @@
             <fieldset>
                 <legend>Guides</legend>
 
-                <label>Nom du guide
-                    <input type="checkbox" name="guide[]" value="id_guide">
-                </label>
                 <?php
-                    //récupérer les guides (dispo aux dates?)
+                foreach($guides as $guide){
+                    echo "<label>" . $guide['first_name'] . " " . $guide['last_name'] .
+                    "<input type='checkbox' name='guide[]' value='" . $guide['id'] . "'>";
+                }
                 ?>
+                </label>
             </fieldset>
             
             <input type="submit" value="Créer l'excursion">
